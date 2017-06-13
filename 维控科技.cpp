03575ae6a9ea9ee19fc14c754e5a17b2c27dc539
VGxiaozhao <zuodiya@163.com>
2017-06-13 23:19:07 +0800

    Update 维控科技.cpp

--- 维控科技.cpp
+++ 维控科技.cpp
@@ -6,117 +6,89 @@
 using namespace std;
 typedef  long long LL;
 
-
 class CBox
 {
 public:
-    CBox(){}
-    ~CBox(){}
-    CBox(int height,int index,LL sum,LL ans):
-        m_nHeight(height),
-        m_nIndex(index),
-        m_lSum(sum),
-        m_lAns(ans)
-        {
+	CBox(){}
+	~CBox(){}
+	CBox(int height, int index, LL sum, LL ans) :
+		m_nHeight(height),
+		m_nIndex(index),
+		m_lSum(sum),
+		m_lAns(ans){}
 
-        }
-    int getHeight()
-    {
-        return m_nIndex;
-    }
+	int getHeight()
+	{
+		return m_nHeight;
+	}
 
-    LL getAns()
-    {
-        return m_lAns;
-    }
+	LL getAns()
+	{
+		return m_lAns;
+	}
 
-    int getIndex()
-    {
-        return m_nIndex;
-    }
+	int getIndex()
+	{
+		return m_nIndex;
+	}
 
-    LL getSum()
-    {
-        return m_lSum;
-    }
+	LL getSum()
+	{
+		return m_lSum;
+	}
+
 private:
-    int m_nHeight,m_nIndex;
-    LL m_lSum,m_lAns;
+	int m_nHeight, m_nIndex;
+	LL m_lSum, m_lAns;
 };
 
 class CSolver
 {
 public:
-    CSolver()
-    {
-        while(!m_st.empty())
-            m_st.pop();
-    }
-    ~CSolver()
-    {
-        while(!m_st.empty())
-            m_st.pop();
-    }
-    static void init()
-    {
-        while(!m_st.empty())
-            m_st.pop();
-    }
-    static LL getAns(int a[],int st,int ed)
-    {
-        LL ret = 0,sum = 0;
-        m_st.push(CBox(a[st],st,0,0));
-        sum = a[st];
-        for(int i=st+1; i<=ed; i++)
-        {
-            CBox* last = NULL;
-            while(!m_st.empty() && (last=&m_st.top())->getHeight() <=a[i])
-                m_st.pop();
-            LL temp=0;
-            if(last->getIndex()+1<i)
-                temp = last->getAns() + (i-last->getIndex()-1)*min(last->getHeight(),a[i]) - ( sum-last->getSum());
-            else
-                temp = last->getAns();
-            sum = sum + a[i];
-            m_st.push(CBox(a[i],i,sum,temp));
-            ret = max(ret,temp);
-        }
-        return ret;
-    }
+	static void init()
+	{
+		while (!m_st.empty())
+			m_st.pop();
+	}
+	static LL getAns(int a[], int st, int ed)
+	{
+		init();
+		LL ret = 0, sum = 0;
+		m_st.push(CBox(a[st], st, a[st], 0));
+		sum = a[st];
+		for (int i = st + 1; i <= ed; i++)
+		{
+			CBox* last = NULL;		//记录单调栈最后一个访问的元素
+			while (!m_st.empty() && (last = &m_st.top())->getHeight() <= a[i])			//当栈顶比当前小，说明可以形成坑，弹出元素
+				m_st.pop();
+			LL temp = 0;
+			if (last->getIndex() + 1 < i)			//这样表示可以形成坑
+				temp = last->getAns() + (i - last->getIndex() - 1) * min(last->getHeight(), a[i]) - (sum - last->getSum());
+			else
+				temp = last->getAns();
+			sum = sum + a[i];
+			m_st.push(CBox(a[i], i, sum, temp));
+			ret = max(ret, temp);
+		}
+		return ret;
+	}
 private:
-    static stack<CBox> m_st;
+	static stack<CBox> m_st;
 };
 
+stack<CBox> CSolver::m_st;
 
 int main()
 {
-	//freopen("d:\\in.txt", "r", stdin);
+	freopen("d:\\test.txt", "r", stdin);
 	int n;
 	while (scanf("%d", &n) != EOF)
 	{
-	    int* a = new int[n+1];
+		int* a = new int[n + 1];
 		for (int i = 1; i <= n; i++)
 			scanf("%d", &a[i]);
-        cout<<CSolver::getAns(a,1,n)<<endl;
-        delete[] a;
-//		stack<int> st;
-//		LL mxans = 0;
-//		memset(ans, 0, sizeof(ans));
-//		while (!st.empty()) st.pop();
-//		for (int i = 1; i <= n; i++)
-//		{
-//			sum[i] = sum[i - 1] + a[i];
-//			int last = i-1;
-//			while (!st.empty() && a[last = st.top()] <= a[i])
-//				st.pop();
-//			if (last + 1 < i)		//说明有夹缝
-//				ans[i] = ans[last] + (i - last-1)*min(a[last], a[i]) - (sum[i - 1] - sum[last]);
-//			else
-//				ans[i] = ans[i - 1];
-//			mxans = max(mxans, ans[i]);
-//			st.push(i);
-//		}
-//		cout << mxans << endl;
+		cout << CSolver::getAns(a, 1, n) << endl;
+		delete[] a;
 	}
 	return 0;
 }
